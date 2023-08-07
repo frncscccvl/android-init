476e0364050e360814ed81500108367b9d9932ea
francis_caccavale <hello@frncscccvl.com>
2023-08-07 16:40:58 -0400

    update .

---
+++ app/src/main/java/com/example/android_init/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.android_init
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            
+        }
+    }
+}
